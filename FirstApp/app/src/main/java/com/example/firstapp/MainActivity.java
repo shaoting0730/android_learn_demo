4f183c29ad01dbd1aad570ad8df0bd3b8e8a699a
zhoushaoting <510738319@qq.com>
2024-05-08 11:27:37 +0800

    login登录UI

--- FirstApp/app/src/main/java/com/example/firstapp/MainActivity.java
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.firstapp.activity.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this,
+                        LoginActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
